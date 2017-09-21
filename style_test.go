e5e7b57c46d6290a92e7b9d3c4048bdf90d6a2b2
Alec Thomas <alec@swapoff.org>
2017-09-21 12:54:18 +1000

    Fix style tests.

--- style_test.go
+++ style_test.go
@@ -7,16 +7,18 @@ import (
 )
 
 func TestStyleInherit(t *testing.T) {
-	s := NewStyle("test", StyleEntries{
+	s, err := NewStyle("test", StyleEntries{
 		Name:         "bold #f00",
 		NameVariable: "#fff",
 	})
+	require.NoError(t, err)
 	require.Equal(t, &StyleEntry{Colour: 0x1000000, Bold: true}, s.Get(NameVariable))
 }
 
 func TestColours(t *testing.T) {
-	s := NewStyle("test", StyleEntries{
+	s, err := NewStyle("test", StyleEntries{
 		Name: "#f00 bg:#001 border:#ansiblue",
 	})
+	require.NoError(t, err)
 	require.Equal(t, &StyleEntry{Colour: 0xff0001, Background: 0x000012, Border: 0x000100}, s.Get(Name))
 }
