7cc9ea63f558b4f9464945491a213cd2d12bc640
luis <stdiopt@gmail.com>
2017-10-17 01:50:04 -0200

    Changed terminal16m to semicolons
    Most terminals seems to use semicolons in true colors:
    https://gist.github.com/XVilka/8346728

--- formatters/tty_truecolour.go
+++ formatters/tty_truecolour.go
@@ -22,10 +22,10 @@ func trueColourFormatter(w io.Writer, style *chroma.Style, it chroma.Iterator) e
 				out += "\033[4m"
 			}
 			if entry.Colour.IsSet() {
-				out += fmt.Sprintf("\033[38:2:%d:%d:%dm", entry.Colour.Red(), entry.Colour.Green(), entry.Colour.Blue())
+				out += fmt.Sprintf("\033[38;2;%d;%d;%dm", entry.Colour.Red(), entry.Colour.Green(), entry.Colour.Blue())
 			}
 			if entry.Background.IsSet() {
-				out += fmt.Sprintf("\033[48:2:%d:%d:%dm", entry.Background.Red(), entry.Background.Green(), entry.Background.Blue())
+				out += fmt.Sprintf("\033[48;2;%d;%d;%dm", entry.Background.Red(), entry.Background.Green(), entry.Background.Blue())
 			}
 			fmt.Fprint(w, out)
 		}
