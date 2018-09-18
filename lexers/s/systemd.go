f1009fe766f03d83ad03043dc97a54b61b292626
Peter Lamby <peterlamby@web.de>
2018-09-18 07:19:18 +0200

    Use Text marker for remaining line endings
    This is already done for most line endіngs

--- lexers/s/systemd.go
+++ lexers/s/systemd.go
@@ -17,11 +17,11 @@ var SYSTEMD = internal.Register(MustNewLexer(
 			{`\s+`, Text, nil},
 			{`[;#].*`, Comment, nil},
 			{`\[.*?\]$`, Keyword, nil},
-			{`(.*?)(=)(.*)(\\\n)`, ByGroups(NameAttribute, Operator, LiteralString, None), Push("continuation")},
+			{`(.*?)(=)(.*)(\\\n)`, ByGroups(NameAttribute, Operator, LiteralString, Text), Push("continuation")},
 			{`(.*?)(=)(.*)`, ByGroups(NameAttribute, Operator, LiteralString), nil},
 		},
 		"continuation": {
-			{`(.*?)(\\\n)`, ByGroups(LiteralString, None), nil},
+			{`(.*?)(\\\n)`, ByGroups(LiteralString, Text), nil},
 			{`(.*)`, LiteralString, Pop(1)},
 		},
 	},
