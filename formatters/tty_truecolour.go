53ec482d87b3e2cf59adc85dfe45565c44457c37
Ray Lee <ray@evq.io>
2019-09-01 12:26:47 -0700

    Add italics support for truecolour terminals

--- formatters/tty_truecolour.go
+++ formatters/tty_truecolour.go
@@ -21,6 +21,9 @@ func trueColourFormatter(w io.Writer, style *chroma.Style, it chroma.Iterator) e
 			if entry.Underline == chroma.Yes {
 				out += "\033[4m"
 			}
+			if entry.Italic == chroma.Yes {
+				out += "\033[3m"
+			}
 			if entry.Colour.IsSet() {
 				out += fmt.Sprintf("\033[38;2;%d;%d;%dm", entry.Colour.Red(), entry.Colour.Green(), entry.Colour.Blue())
 			}
