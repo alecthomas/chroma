74e663a388720cdc1ecbc102dd938350bd8a1085
gribok <40306040+gribok@users.noreply.github.com>
2020-04-14 07:43:12 +0000

    Alphabetise all keywords in yang lexer

--- lexers/y/yang.go
+++ lexers/y/yang.go
@@ -35,28 +35,27 @@ var YANG = internal.Register(MustNewLexer(
 			//TOP_STMTS_KEYWORDS
 			{Words(``, `(?=[^\w\-\:])`, `module`, `submodule`), Keyword, nil},
 			//MODULE_HEADER_STMT_KEYWORDS
-			{Words(``, `(?=[^\w\-\:])`, `yang-version`, `namespace`, `prefix`, `belongs-to`), Keyword, nil},
+			{Words(``, `(?=[^\w\-\:])`, `belongs-to`, `namespace`, `prefix`, `yang-version`), Keyword, nil},
 			//META_STMT_KEYWORDS
-			{Words(``, `(?=[^\w\-\:])`, `organization`, `contact`, `description`, `reference`, `revision`), Keyword, nil},
+			{Words(``, `(?=[^\w\-\:])`, `contact`, `description`, `organization`, `reference`, `revision`), Keyword, nil},
 			//LINKAGE_STMTS_KEYWORDS
 			{Words(``, `(?=[^\w\-\:])`, `import`, `include`, `revision-date`), Keyword, nil},
 			//BODY_STMT_KEYWORDS
-			{Words(``, `(?=[^\w\-\:])`, `extension`, `feature`, `identity`, `typedef`, `grouping`, `augment`, `rpc`, `notification`, `deviation`, `action`, `argument`, `if-feature`, `input`, `output`), Keyword, nil},
+			{Words(``, `(?=[^\w\-\:])`, `action`, `argument`, `augment`, `deviation`, `extension`, `feature`, `grouping`, `identity`, `if-feature`, `input`, `notification`, `output`, `rpc`, `typedef`), Keyword, nil},
 			//DATA_DEF_STMT_KEYWORDS
-			{Words(``, `(?=[^\w\-\:])`, `container`, `leaf-list`, `leaf`, `list`, `choice`, `anydata`, `anyxml`, `uses`, `case`, `config`, `deviate`, `must`, `when`, `presence`, `refine`), Keyword, nil},
+			{Words(``, `(?=[^\w\-\:])`, `anydata`, `anyxml`, `case`, `choice`, `config`, `container`, `deviate`, `leaf`, `leaf-list`, `list`, `must`, `presence`, `refine`, `uses`, `when`), Keyword, nil},
 			//TYPE_STMT_KEYWORDS
-			{Words(``, `(?=[^\w\-\:])`, `type`, `units`, `default`, `status`, `bit`, `enum`, `error-app-tag`, `error-message`, `fraction-digits`, `length`, `min-elements`, `max-elements`, `modifier`, `ordered-by`, `path`, `pattern`, `position`, `range`, `require-instance`, `value`, `yin-element`, `base`), Keyword, nil},
+			{Words(``, `(?=[^\w\-\:])`, `base`, `bit`, `default`, `enum`, `error-app-tag`, `error-message`, `fraction-digits`, `length`, `max-elements`, `min-elements`, `modifier`, `ordered-by`, `path`, `pattern`, `position`, `range`, `require-instance`, `status`, `type`, `units`, `value`, `yin-element`), Keyword, nil},
 			//LIST_STMT_KEYWORDS
 			{Words(``, `(?=[^\w\-\:])`, `key`, `mandatory`, `unique`), Keyword, nil},
 
 			//CONSTANTS_KEYWORDS - RFC7950 other keywords
-			{Words(``, `(?=[^\w\-\:])`, `true`, `false`, `current`, `obsolete`, `deprecated`, `add`, `delete`, `replace`, `not-supported`, `invert-match`, `max`, `min`, `unbounded`, `user`), NameClass, nil},
+			{Words(``, `(?=[^\w\-\:])`, `add`, `current`, `delete`, `deprecated`, `false`, `invert-match`, `max`, `min`, `not-supported`, `obsolete`, `replace`, `true`, `unbounded`, `user`), NameClass, nil},
 
 			//RFC7950 Built-In Types
-			{Words(``, `(?=[^\w\-\:])`, `binary`, `bits`, `boolean`, `decimal64`, `empty`, `enumeration`, `int8`, `int16`, `int32`, `int64`, `string`, `uint8`, `uint16`, `uint32`, `uint64`, `union`, `leafref`, `identityref`, `instance-identifier`), NameClass, nil},
+			{Words(``, `(?=[^\w\-\:])`, `binary`, `bits`, `boolean`, `decimal64`, `empty`, `enumeration`, `identityref`, `instance-identifier`, `int16`, `int32`, `int64`, `int8`, `leafref`, `string`, `uint16`, `uint32`, `uint64`, `uint8`, `union`), NameClass, nil},
 
-
-			{`[^;{}\s\'\"]+`, Text ,nil},
+			{`[^;{}\s\'\"]+`, Text, nil},
 		},
 		"comments": {
 			{`[^*/]`, CommentMultiline, nil},
