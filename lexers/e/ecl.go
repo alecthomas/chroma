e13655c887516bf1c4a95ff1722c160ed7518c27
Carlos Henrique Guardão Gandarez <gandarez@gmail.com>
2020-11-20 19:13:40 -0300

    Add ECL lexer text analysis
    Add test to detect "->" into ECL llexer

--- lexers/e/ecl.go
+++ lexers/e/ecl.go
@@ -1,6 +1,8 @@
 package e
 
 import (
+	"strings"
+
 	. "github.com/alecthomas/chroma" // nolint
 	"github.com/alecthomas/chroma/lexers/internal"
 )
@@ -16,4 +18,23 @@ var Ecl = internal.Register(MustNewLexer(
 	Rules{
 		"root": {},
 	},
-))
+).SetAnalyser(func(text string) float32 {
+	// This is very difficult to guess relative to other business languages.
+	// -> in conjunction with BEGIN/END seems relatively rare though.
+
+	var result float32 = 0
+
+	if strings.Contains(text, "->") {
+		result += 0.01
+	}
+
+	if strings.Contains(text, "BEGIN") {
+		result += 0.01
+	}
+
+	if strings.Contains(text, "END") {
+		result += 0.01
+	}
+
+	return result
+}))
