16478af4de8cc3aa99460200acfa8b213597cafa
Carlos Henrique Guardão Gandarez <gandarez@gmail.com>
2020-11-24 16:32:22 -0300

    Add Hybris lexer text analysis
    Add Hybris to TODO list

--- lexers/h/hybris.go
+++ lexers/h/hybris.go
@@ -1,9 +1,13 @@
 package h
 
 import (
+	"regexp"
+
 	. "github.com/alecthomas/chroma" // nolint
 	"github.com/alecthomas/chroma/lexers/internal"
 )
+
+var hybrisAnalyserRe = regexp.MustCompile(`\b(?:public|private)\s+method\b`)
 
 // Hybris lexer.
 var Hybris = internal.Register(MustNewLexer(
@@ -16,4 +20,12 @@ var Hybris = internal.Register(MustNewLexer(
 	Rules{
 		"root": {},
 	},
-))
+).SetAnalyser(func(text string) float32 {
+	// public method and private method don't seem to be quite common
+	// elsewhere.
+	if hybrisAnalyserRe.MatchString(text) {
+		return 0.01
+	}
+
+	return 0
+}))
