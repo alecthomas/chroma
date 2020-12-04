42430bcd9c9b3bf8bf99cc3fcc1dcf7b06c1f9a2
Carlos Henrique Guardão Gandarez <gandarez@gmail.com>
2020-12-03 21:35:41 -0300

    Add Nemerle lexer text analysis

--- lexers/n/nemerle.go
+++ lexers/n/nemerle.go
@@ -1,6 +1,8 @@
 package n
 
 import (
+	"strings"
+
 	. "github.com/alecthomas/chroma" // nolint
 	"github.com/alecthomas/chroma/lexers/internal"
 )
@@ -17,4 +19,12 @@ var Nemerle = internal.Register(MustNewLexer(
 	Rules{
 		"root": {},
 	},
-))
+).SetAnalyser(func(text string) float32 {
+	// Nemerle is quite similar to Python, but @if is relatively uncommon
+	// elsewhere.
+	if strings.Contains(text, "@if") {
+		return 0.1
+	}
+
+	return 0
+}))
