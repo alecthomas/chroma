12ddb8f19b3c69c1597d472feba6e80f04da0c99
andy <andy@fastback.io>
2020-12-04 01:28:35 +0100

    Move 2 lexers to distinct files

--- lexers/g/gosu.go
+++ lexers/g/gosu.go
@@ -17,16 +17,3 @@ var Gosu = internal.Register(MustNewLexer(
 		"root": {},
 	},
 ))
-
-// GosuTemplate lexer.
-var GosuTemplate = internal.Register(MustNewLexer(
-	&Config{
-		Name:      "Gosu Template",
-		Aliases:   []string{"gst"},
-		Filenames: []string{"*.gst"},
-		MimeTypes: []string{"text/x-gosu-template"},
-	},
-	Rules{
-		"root": {},
-	},
-))
