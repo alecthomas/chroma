a32ab1d8716524f0bb84726e733140cb1873adce
Carlos Henrique Guardão Gandarez <gandarez@gmail.com>
2020-12-12 19:06:03 -0300

    Add GAP lexer text analysis

--- lexers/g/gap.go
+++ lexers/g/gap.go
@@ -1,8 +1,18 @@
 package g
 
 import (
+	"math"
+	"regexp"
+
 	. "github.com/alecthomas/chroma" // nolint
 	"github.com/alecthomas/chroma/lexers/internal"
+)
+
+var (
+	gapAnalyserDeclarationRe = regexp.MustCompile(
+		`(InstallTrueMethod|Declare(Attribute|Category|Filter|Operation|GlobalFunction|Synonym|SynonymAttr|Property))`)
+	gapAnalyserImplementationRe = regexp.MustCompile(
+		`(DeclareRepresentation|Install(GlobalFunction|Method|ImmediateMethod|OtherMethod)|New(Family|Type)|Objectify)`)
 )
 
 // GAP lexer.
@@ -16,4 +26,16 @@ var Gap = internal.Register(MustNewLexer(
 	Rules{
 		"root": {},
 	},
-))
+).SetAnalyser(func(text string) float32 {
+	var result float64
+
+	if gapAnalyserDeclarationRe.MatchString(text) {
+		result += 0.7
+	}
+
+	if gapAnalyserImplementationRe.MatchString(text) {
+		result += 0.7
+	}
+
+	return float32(math.Min(result, float64(1.0)))
+}))
