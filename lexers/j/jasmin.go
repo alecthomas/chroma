dd1e510a76c9599f9a9bfeae053aa7a0318ba5be
Carlos Henrique Guardão Gandarez <gandarez@gmail.com>
2020-12-12 10:17:09 -0300

    Add Jasmin lexer text analysis

--- lexers/j/jasmin.go
+++ lexers/j/jasmin.go
@@ -1,8 +1,17 @@
 package j
 
 import (
+	"regexp"
+
 	. "github.com/alecthomas/chroma" // nolint
 	"github.com/alecthomas/chroma/lexers/internal"
+)
+
+var (
+	jasminAnalyserClassRe       = regexp.MustCompile(`(?m)^\s*\.class\s`)
+	jasminAnalyserInstructionRe = regexp.MustCompile(`(?m)^\s*[a-z]+_[a-z]+\b`)
+	jasminAnalyserKeywordsRe    = regexp.MustCompile(
+		`(?m)^\s*\.(attribute|bytecode|debug|deprecated|enclosing|inner|interface|limit|set|signature|stack)\b`)
 )
 
 // Jasmin lexer.
@@ -15,4 +24,20 @@ var Jasmin = internal.Register(MustNewLexer(
 	Rules{
 		"root": {},
 	},
-))
+).SetAnalyser(func(text string) float32 {
+	var result float32
+
+	if jasminAnalyserClassRe.MatchString(text) {
+		result += 0.5
+
+		if jasminAnalyserInstructionRe.MatchString(text) {
+			result += 0.3
+		}
+	}
+
+	if jasminAnalyserKeywordsRe.MatchString(text) {
+		result += 0.6
+	}
+
+	return result
+}))
