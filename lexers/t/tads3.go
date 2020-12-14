d85a0f57ecadb29212c20dc28a290a9cd6e5b240
Carlos Henrique Guardão Gandarez <gandarez@gmail.com>
2020-12-14 17:02:45 -0300

    Add TADS 3 lexer text analysis

--- lexers/t/tads3.go
+++ lexers/t/tads3.go
@@ -1,6 +1,8 @@
 package t
 
 import (
+	"strings"
+
 	. "github.com/alecthomas/chroma" // nolint
 	"github.com/alecthomas/chroma/lexers/internal"
 )
@@ -15,4 +17,20 @@ var Tads3 = internal.Register(MustNewLexer(
 	Rules{
 		"root": {},
 	},
-))
+).SetAnalyser(func(text string) float32 {
+	// This is a rather generic descriptive language without strong
+	// identifiers. It looks like a 'GameMainDef' has to be present,
+	// and/or a 'versionInfo' with an 'IFID' field.
+	var result float32
+
+	if strings.Contains(text, "__TADS") || strings.Contains(text, "GameMainDef") {
+		result += 0.2
+	}
+
+	// This is a fairly unique keyword which is likely used in source as well.
+	if strings.Contains(text, "versionInfo") && strings.Contains(text, "IFID") {
+		result += 0.1
+	}
+
+	return result
+}))
