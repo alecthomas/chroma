577296d7a47c822d7e5549c241a2c00c9d5f9c53
Carlos Henrique Guardão Gandarez <gandarez@gmail.com>
2020-12-14 19:31:00 -0300

    Add ucode lexer text analysis

--- lexers/u/ucode.go
+++ lexers/u/ucode.go
@@ -1,6 +1,8 @@
 package u
 
 import (
+	"strings"
+
 	. "github.com/alecthomas/chroma" // nolint
 	"github.com/alecthomas/chroma/lexers/internal"
 )
@@ -15,4 +17,33 @@ var Ucode = internal.Register(MustNewLexer(
 	Rules{
 		"root": {},
 	},
-))
+).SetAnalyser(func(text string) float32 {
+	// endsuspend and endrepeat are unique to this language, and
+	// \self, /self doesn't seem to get used anywhere else either.
+	var result float32
+
+	if strings.Contains(text, "endsuspend") {
+		result += 0.1
+	}
+
+	if strings.Contains(text, "endrepeat") {
+		result += 0.1
+	}
+
+	if strings.Contains(text, ":=") {
+		result += 0.01
+	}
+
+	if strings.Contains(text, "procedure") && strings.Contains(text, "end") {
+		result += 0.01
+	}
+
+	// This seems quite unique to unicon -- doesn't appear in any other
+	// example source we have (A quick search reveals that \SELF appears in
+	// Perl/Raku code)
+	if strings.Contains(text, `\self`) && strings.Contains(text, "/self") {
+		result += 0.5
+	}
+
+	return result
+}))
