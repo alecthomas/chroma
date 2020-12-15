1184ebe7604bd2003ae824eec6e5369d9c0e19f4
Carlos Henrique Guardão Gandarez <gandarez@gmail.com>
2020-12-15 19:51:21 -0300

    Add UrbriScript lexer text analysis

--- lexers/u/urbiscript.go
+++ lexers/u/urbiscript.go
@@ -1,6 +1,8 @@
 package u
 
 import (
+	"strings"
+
 	. "github.com/alecthomas/chroma" // nolint
 	"github.com/alecthomas/chroma/lexers/internal"
 )
@@ -16,4 +18,18 @@ var UrbiScript = internal.Register(MustNewLexer(
 	Rules{
 		"root": {},
 	},
-))
+).SetAnalyser(func(text string) float32 {
+	// This is fairly similar to C and others, but freezeif and
+	// waituntil are unique keywords.
+	var result float32
+
+	if strings.Contains(text, "freezeif") {
+		result += 0.05
+	}
+
+	if strings.Contains(text, "waituntil") {
+		result += 0.05
+	}
+
+	return result
+}))
