ba02b1eb46914f5b171465b2bea54de556b3b5c2
Carlos Henrique Guardão Gandarez <gandarez@gmail.com>
2021-01-08 17:50:09 -0300

    Add HTTP lexer text analysis

---
+++ lexers/h/http_test.go
@@ -0,0 +1,20 @@
+package h_test
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/alecthomas/assert"
+	"github.com/alecthomas/chroma"
+	"github.com/alecthomas/chroma/lexers/h"
+)
+
+func TestHTTP_AnalyseText(t *testing.T) {
+	data, err := ioutil.ReadFile("testdata/http_request.http")
+	assert.NoError(t, err)
+
+	analyser, ok := h.HTTP.(chroma.Analyser)
+	assert.True(t, ok)
+
+	assert.Equal(t, float32(1.0), analyser.AnalyseText(string(data)))
+}
