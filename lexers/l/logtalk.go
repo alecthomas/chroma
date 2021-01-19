c6ab61c55a520d0123e9fcff04a711d3bbf71e81
Carlos Henrique Guardão Gandarez <gandarez@gmail.com>
2021-01-19 16:58:24 -0300

    Logtalk lexer text analysis

--- lexers/l/logtalk.go
+++ lexers/l/logtalk.go
@@ -1,9 +1,14 @@
 package l
 
 import (
+	"regexp"
+	"strings"
+
 	. "github.com/alecthomas/chroma" // nolint
 	"github.com/alecthomas/chroma/lexers/internal"
 )
+
+var logtalkAnalyserSyntaxRe = regexp.MustCompile(`(?m)^:-\s[a-z]`)
 
 // Logtalk lexer.
 var Logtalk = internal.Register(MustNewLexer(
@@ -16,4 +21,16 @@ var Logtalk = internal.Register(MustNewLexer(
 	Rules{
 		"root": {},
 	},
-))
+).SetAnalyser(func(text string) float32 {
+	if strings.Contains(text, ":- object(") ||
+		strings.Contains(text, ":- protocol(") ||
+		strings.Contains(text, ":- category(") {
+		return 1.0
+	}
+
+	if logtalkAnalyserSyntaxRe.MatchString(text) {
+		return 0.9
+	}
+
+	return 0
+}))
