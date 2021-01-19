fa0f62b3d7c9d80bcfb69595b923c675e3bc6942
Carlos Henrique Guardão Gandarez <gandarez@gmail.com>
2021-01-19 09:57:27 -0300

    Java Server Page lexer text analysis

--- lexers/j/jsp.go
+++ lexers/j/jsp.go
@@ -1,8 +1,12 @@
 package j
 
 import (
+	"strings"
+
+	"github.com/alecthomas/chroma"
 	. "github.com/alecthomas/chroma" // nolint
 	"github.com/alecthomas/chroma/lexers/internal"
+	"github.com/alecthomas/chroma/pkg/xml"
 )
 
 // Jsp lexer.
@@ -16,4 +20,20 @@ var Jsp = internal.Register(MustNewLexer(
 	Rules{
 		"root": {},
 	},
-))
+).SetAnalyser(func(text string) float32 {
+	var result float32
+
+	if analyser, ok := Java.(chroma.Analyser); ok {
+		result = analyser.AnalyseText(text) - 0.01
+	}
+
+	if xml.MatchString(text) {
+		result += 0.4
+	}
+
+	if strings.Contains(text, "<%") && strings.Contains(text, "%>") {
+		result += 0.1
+	}
+
+	return result
+}))
