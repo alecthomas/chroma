719edd02e0a785088c466eca783e699b7855f297
Carlos Henrique Guardão Gandarez <gandarez@gmail.com>
2021-01-22 13:16:49 -0300

    NumPy lexer text analysis

--- lexers/n/numpy.go
+++ lexers/n/numpy.go
@@ -1,8 +1,11 @@
 package n
 
 import (
+	"strings"
+
 	. "github.com/alecthomas/chroma" // nolint
 	"github.com/alecthomas/chroma/lexers/internal"
+	"github.com/alecthomas/chroma/pkg/shebang"
 )
 
 // NumPy lexer.
@@ -14,4 +17,22 @@ var NumPy = internal.Register(MustNewLexer(
 	Rules{
 		"root": {},
 	},
-))
+).SetAnalyser(func(text string) float32 {
+	hasPythonShebang, _ := shebang.MatchString(text, `pythonw?(3(\.\d)?)?`)
+	containsNumpyImport := strings.Contains(text, "import numpy")
+	containsFromNumpyImport := strings.Contains(text, "from numpy import")
+
+	var containsImport bool
+
+	if len(text) > 1000 {
+		containsImport = strings.Contains(text[:1000], "import ")
+	} else {
+		containsImport = strings.Contains(text, "import ")
+	}
+
+	if (hasPythonShebang || containsImport) && (containsNumpyImport || containsFromNumpyImport) {
+		return 1.0
+	}
+
+	return 0
+}))
