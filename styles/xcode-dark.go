49d1c236cb5720568c3b4dd49146da9074bdd586
Daichi Hayashi <daichikuwa0618@gmail.com>
2021-03-21 18:20:46 +0900

    add: color variables from Xcode

---
+++ styles/xcode-dark.go
@@ -0,0 +1,37 @@
+package styles
+
+import (
+	"github.com/alecthomas/chroma"
+)
+
+var (
+	// Inspired by Apple's Xcode "Default (Dark)" Theme
+	background                         = "#1F1F24"
+	plainText                          = "#FFFFFF"
+	comments                           = "#6C7986"
+	documentationMarkup                = "#6C7986"
+	documentationMarkupKeywords        = "#92A1B1"
+	marks                              = "#92A1B1"
+	strings                            = "#FC6A5D"
+	characters                         = "#D0BF69"
+	numbers                            = "#D0BF69"
+	keywords                           = "#FC5FA3"
+	preprocessorStatements             = "#FD8F3F"
+	urls                               = "#5482FF"
+	attributes                         = "#BF8555"
+	typeDeclarations                   = "#5DD8FF"
+	otherDeclarations                  = "#41A1C0"
+	projectClassNames                  = "#9EF1DD"
+	projectFunctionAndMethodNames      = "#67B7A4"
+	projectConstants                   = "#67B7A4"
+	projectTypeNames                   = "#9EF1DD"
+	projectInstanceVariablesAndGlobals = "#67B7A4"
+	projectPreprocessorMacro           = "#FD8F3F"
+	otherClassNames                    = "#D0A8FF"
+	otherFunctionAndMethodNames        = "#A167E6"
+	otherConstants                     = "#A167E6"
+	otherTypeNames                     = "#D0A8FF"
+	otherInstanceVariablesAndGlobals   = "#A167E6"
+	otherPreprocessorMacros            = "#FD8F3F"
+	heading                            = "#AA0D91"
+)
