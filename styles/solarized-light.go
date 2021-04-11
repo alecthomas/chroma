a432989fb68da3171e7870f317a47cbf4eb2a9fa
Zhangyuan Nie <yuan@znie.org>
2021-04-11 02:23:52 -0400

    better solarized light

--- styles/solarized-light.go
+++ styles/solarized-light.go
@@ -5,20 +5,53 @@ import (
 )
 
 // SolarizedLight style.
-var SolarizedLight = Register(chroma.MustNewStyle("solarized-light", chroma.StyleEntries{
-	chroma.Text:             "bg: #eee8d5 #586e75",
-	chroma.Keyword:          "#859900",
-	chroma.KeywordConstant:  "bold",
-	chroma.KeywordNamespace: "#dc322f bold",
-	chroma.KeywordType:      "bold",
-	chroma.Name:             "#268bd2",
-	chroma.NameBuiltin:      "#cb4b16",
-	chroma.NameClass:        "#cb4b16",
-	chroma.NameTag:          "bold",
-	chroma.Literal:          "#2aa198",
-	chroma.LiteralNumber:    "bold",
-	chroma.OperatorWord:     "#859900",
-	chroma.Comment:          "#93a1a1 italic",
-	chroma.Generic:          "#d33682",
-	chroma.Background:       " bg:#eee8d5",
-}))
+var SolarizedLight = Register(chroma.MustNewStyle("solarized-light", func() chroma.StyleEntries {
+	var (
+		base00  = "#657b83"
+		base0   = "#839496"
+		base1   = "#93a1a1"
+		base2   = "#eee8d5"
+		base3   = "#fdf6e3"
+		yellow  = "#b58900"
+		orange  = "#cb4b16"
+		red     = "#dc322f"
+		magenta = "#d33682"
+		blue    = "#268bd2"
+		cyan    = "#2aa198"
+		green   = "#859900"
+	)
+
+	return chroma.StyleEntries{
+		chroma.Text:                base00,
+		chroma.Keyword:             green,
+		chroma.KeywordNamespace:    orange,
+		chroma.KeywordDeclaration:  blue,
+		chroma.KeywordType:         yellow,
+		chroma.Name:                base00,
+		chroma.NameBuiltin:         green,
+		chroma.NameTag:             "bold " + blue,
+		chroma.Literal:             cyan,
+		chroma.LiteralStringEscape: orange,
+		chroma.LiteralNumber:       magenta,
+		chroma.LiteralStringRegex:  orange,
+		chroma.OperatorWord:        green,
+		chroma.Comment:             "italic " + base1,
+		chroma.CommentPreproc:      "noitalic " + orange,
+		chroma.CommentPreprocFile:  cyan,
+		chroma.CommentSpecial:      "noitalic " + cyan,
+		chroma.Generic:             base00,
+		chroma.GenericDeleted:      red,
+		chroma.GenericEmph:         "italic",
+		chroma.GenericError:        red,
+		chroma.GenericHeading:      "bold",
+		chroma.GenericSubheading:   "bold",
+		chroma.GenericInserted:     green,
+		chroma.GenericPrompt:       "bold " + blue,
+		chroma.GenericStrong:       "bold",
+		chroma.GenericUnderline:    "underline",
+		chroma.Background:          "bg:" + base3,
+		chroma.LineNumbers:         base0 + " bg:" + base2,
+		chroma.LineHighlight:       "bg:" + base2,
+		chroma.Error:               "bg:" + red,
+	}
+}()))
