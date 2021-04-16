f1785f6ce50cdd14ae26de5c34414a2dbe9950af
andy <andy@fastback.io>
2021-04-16 23:51:44 +0200

    Velocity lexer text analysis

--- lexers/v/velocity.go
+++ lexers/v/velocity.go
@@ -1,8 +1,17 @@
 package v
 
 import (
+	"regexp"
+
 	. "github.com/alecthomas/chroma" // nolint
 	"github.com/alecthomas/chroma/lexers/internal"
+)
+
+var (
+	velocityAnalzserMacroRe     = regexp.MustCompile(`(?s)#\{?macro\}?\(.*?\).*?#\{?end\}?`)
+	velocityAnalzserIfRe        = regexp.MustCompile(`(?s)#\{?if\}?\(.+?\).*?#\{?end\}?`)
+	velocityAnalzserForeachRe   = regexp.MustCompile(`(?s)#\{?foreach\}?\(.+?\).*?#\{?end\}?`)
+	velocityAnalzserReferenceRe = regexp.MustCompile(`\$!?\{?[a-zA-Z_]\w*(\([^)]*\))?(\.\w+(\([^)]*\))?)*\}?`)
 )
 
 // Velocity lexer.
@@ -15,4 +24,24 @@ var Velocity = internal.Register(MustNewLexer(
 	Rules{
 		"root": {},
 	},
-))
+).SetAnalyser(func(text string) float32 {
+	var result float64
+
+	if velocityAnalzserMacroRe.MatchString(text) {
+		result += 0.25
+	}
+
+	if velocityAnalzserIfRe.MatchString(text) {
+		result += 0.15
+	}
+
+	if velocityAnalzserForeachRe.MatchString(text) {
+		result += 0.15
+	}
+
+	if velocityAnalzserReferenceRe.MatchString(text) {
+		result += 0.01
+	}
+
+	return float32(result)
+}))
