a2d7c3b7e63f8ee657c912ef113a8af0b4c82424
Siavash Askari Nasr <siavash.askari.nasr@gmail.com>
2021-04-27 22:52:13 +0430

    Improve Svelte lexer

--- lexers/s/svelte.go
+++ lexers/s/svelte.go
@@ -22,18 +22,52 @@ var Svelte = internal.Register(DelegatingLexer(h.HTML, MustNewLazyLexer(
 func svelteRules() Rules {
 	return Rules{
 		"root": {
-			{`(<\s*script\s*lang\s*=\s*['"](?:ts|typescript)['"]\s*>)(.+?)(<\s*/\s*script\s*>)`, ByGroups(Other, Using(t.TypeScript), Other), nil},
-			{`\{`, Punctuation, Push("templates")},
-			{`[^{]`, Other, nil},
+			// Let HTML handle the comments, including comments containing script and style tags
+			{`<!--`, Other, Push("comment")},
+			{
+				// Highlight script and style tags based on lang attribute
+				// and allow attributes besides lang
+				`(<\s*(?:script|style).*?lang\s*=\s*['"])` +
+				`(.+?)(['"].*?>)` +
+				`(.+?)` +
+				`(<\s*/\s*(?:script|style)\s*>)`,
+				UsingByGroup(internal.Get, 2, 4, Other, Other, Other, Other, Other),
+				nil,
+			},
+			{
+				// Make sure `{` is not inside script or style tags
+				`(?<!<\s*(?:script|style)(?:(?!(?:script|style)\s*>).)*?)` +
+				`{` +
+				`(?!(?:(?!<\s*(?:script|style)).)*?(?:script|style)\s*>)`,
+				Punctuation,
+				Push("templates"),
+			},
+			// on:submit|preventDefault
+			{`(?<=\s+on:\w+(?:\|\w+)*)\|(?=\w+)`, Operator , nil},
+			{`.+?`, Other, nil},
+		},
+		"comment": {
+			{`-->`, Other, Pop(1)},
+			{`.+?`, Other, nil},
 		},
 		"templates": {
 			{`}`, Punctuation, Pop(1)},
+			// Let TypeScript handle strings and the curly braces inside them
+			{`(?<!(?<!\\)\\)(['"` + "`])" + `.*?(?<!(?<!\\)\\)\1`, Using(t.TypeScript), nil},
+			// If there is another opening curly brace push to templates again
+			{"{", Punctuation, Push("templates")},
 			{`@(debug|html)\b`, Keyword, nil},
-			{`(#|/)(await|each|if)\b`, Keyword, nil},
+			{
+				`(#await)(\s+)(\w+)(\s+)(then|catch)(\s+)(\w+)`,
+				ByGroups(Keyword, Text, Using(t.TypeScript), Text,
+					Keyword, Text, Using(t.TypeScript),
+				),
+				nil,
+			},
+			{`(#|/)(await|each|if|key)\b`, Keyword, nil},
 			{`(:else)(\s+)(if)?\b`, ByGroups(Keyword, Text, Keyword), nil},
 			{`:(catch|then)\b`, Keyword, nil},
-			{`then\b`, Keyword, nil},
-			{`[^}]+`, Using(t.TypeScript), nil},
+			{`[^{}]+`, Using(t.TypeScript), nil},
 		},
 	}
 }
