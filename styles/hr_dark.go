e47dbe9db0aadedb47437ebac63f8ea784c277a0
Soubik <soubik.bhui16@gmail.com>
2021-05-22 12:31:22 +0530

    hrdark-styles

--- styles/hr_dark.go
+++ styles/hr_dark.go
@@ -6,10 +6,13 @@ import (
 
 // Theme based on HackerRank Dark Editor theme
 var HrDark = Register(chroma.MustNewStyle("hrdark", chroma.StyleEntries{
-	chroma.Comment : "#828b96",
+	chroma.Comment : "italic #828b96",
 	chroma.Keyword : "#ff636f",
-	chroma.NameKeyword : "#58a1dd",
-
+	chroma.OperatorWord : "#ff636f",
+	chroma.Name : "#58a1dd",
 	chroma.Literal : "#a6be9d",
 	chroma.Operator : "#ff636f",
+	chroma.Error : "",
+	chroma.Background : "#1d2432",
+	chroma.Other : "#fff",
 })) 
