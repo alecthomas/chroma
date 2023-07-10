b9a63eb4f023bae61af7a34bb28543488d838aa9
guangwu <guoguangwu@magic-shield.com>
2023-07-10 16:42:01 +0800

    chore: remove refs to deprecated io/ioutil (#798)

--- formatters/svg/svg.go
+++ formatters/svg/svg.go
@@ -6,7 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"io/ioutil"
+	"os"
 	"path"
 	"strings"
 
@@ -34,7 +34,7 @@ func EmbedFontFile(fontFamily string, fileName string) (option Option, err error
 	}
 
 	var content []byte
-	if content, err = ioutil.ReadFile(fileName); err == nil {
+	if content, err = os.ReadFile(fileName); err == nil {
 		option = EmbedFont(fontFamily, base64.StdEncoding.EncodeToString(content), format)
 	}
 	return
