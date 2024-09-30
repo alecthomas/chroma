2cf0d6730302293b2ffdcf92831c59660bf103a6
Johan Walles <johan.walles@gmail.com>
2024-09-30 07:51:19 +0200

    Simplify two if() statements

--- formatters/tty_truecolour.go
+++ formatters/tty_truecolour.go
@@ -14,29 +14,32 @@ func trueColourFormatter(w io.Writer, style *chroma.Style, it chroma.Iterator) e
 	style = clearBackground(style)
 	for token := it(); token != chroma.EOF; token = it() {
 		entry := style.Get(token.Type)
-		if !entry.IsZero() {
-			out := ""
-			if entry.Bold == chroma.Yes {
-				out += "\033[1m"
-			}
-			if entry.Underline == chroma.Yes {
-				out += "\033[4m"
-			}
-			if entry.Italic == chroma.Yes {
-				out += "\033[3m"
-			}
-			if entry.Colour.IsSet() {
-				out += fmt.Sprintf("\033[38;2;%d;%d;%dm", entry.Colour.Red(), entry.Colour.Green(), entry.Colour.Blue())
-			}
-			if entry.Background.IsSet() {
-				out += fmt.Sprintf("\033[48;2;%d;%d;%dm", entry.Background.Red(), entry.Background.Green(), entry.Background.Blue())
-			}
-			fmt.Fprint(w, out)
+		if entry.IsZero() {
+			fmt.Fprint(w, token.Value)
+			continue
 		}
+
+		out := ""
+		if entry.Bold == chroma.Yes {
+			out += "\033[1m"
+		}
+		if entry.Underline == chroma.Yes {
+			out += "\033[4m"
+		}
+		if entry.Italic == chroma.Yes {
+			out += "\033[3m"
+		}
+		if entry.Colour.IsSet() {
+			out += fmt.Sprintf("\033[38;2;%d;%d;%dm", entry.Colour.Red(), entry.Colour.Green(), entry.Colour.Blue())
+		}
+		if entry.Background.IsSet() {
+			out += fmt.Sprintf("\033[48;2;%d;%d;%dm", entry.Background.Red(), entry.Background.Green(), entry.Background.Blue())
+		}
+		fmt.Fprint(w, out)
+
 		fmt.Fprint(w, token.Value)
-		if !entry.IsZero() {
-			fmt.Fprint(w, "\033[0m")
-		}
+
+		fmt.Fprint(w, "\033[0m")
 	}
 	return nil
 }
